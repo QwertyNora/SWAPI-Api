3907684a4e93fda18365772151e2202f1589c869
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2023-10-05 14:31:25 +0200

    DONE

--- JS/main.js
+++ JS/main.js
@@ -23,26 +23,59 @@
 
 const PLANET_URL = "https://swapi.dev/api/planets/23/";
 
-
 const getPlanetData = async () => {
     try {
         const planetInfoDiv = document.getElementById("planet-info"); 
-        planetInfoDiv.innerHTML = '<p>Loading...</p>'; // Visar loading meddelande
+        planetInfoDiv.innerHTML = '<p class="paragraphLoading">Loading...</p>';
 
         const response = await fetch(PLANET_URL);
         const data = await response.json();
-        console.log(data); // Loggar hämtad data till consol
+        console.log(data);
 
-        // Byter ut loading-message till hämtad data:
+        planetInfoDiv.innerHTML = ''; //Clear modal
         planetInfoDiv.innerHTML = `
-            <h2 class="header">${data?.name}</h2>
+            <h2 class="header">
+                ${data?.name} <button data-close-button class="close-button">&times;</button>
+            </h2>
             <p class="paragraph">Climate: ${data?.climate}</p>
             <p class="paragraph">Terrain: ${data?.terrain}</p>
             <p class="paragraph">Population: ${data?.population}</p>
         `;
+
+        // Attach click event listener to the close button
+        const closeModalButton = document.querySelector('[data-close-button]');
+        closeModalButton.addEventListener('click', () => {
+            closeModal();
+        });
+
     } catch (error) {
         console.log("ERROR: " + error);
         const planetInfoDiv = document.getElementById("planet-info");
         planetInfoDiv.innerHTML = '<p>An error occurred while fetching data.</p>';
     }
 };
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal.active');
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+    const planetInfoDiv = modal.querySelector('#planet-info');
+    planetInfoDiv.innerHTML = ''; // Clear the content
+}
